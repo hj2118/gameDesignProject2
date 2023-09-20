a3033ff305593f18e23bb4128b8fd7a55418bc72
mariaoliv <77213389+mariaoliv@users.noreply.github.com>
2023-09-20 19:52:55 -0400

    changes
    made it so game is over when any of the plater resources" value reaches 0 or 100

--- project2/Assets/Code/PlayerCode.cs
+++ project2/Assets/Code/PlayerCode.cs
@@ -2,33 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCode : MonoBehaviour
 {
-    public Transform feet;
-    public LayerMask ground;
-    public float speed = 10;
-    public int jumpForce = 500;
-    float groundCheckDist = 0.5f;
-    bool grounded = false;
-    Rigidbody2D body;
     
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        grounded = Physics2D.OverlapCircle(feet.position, groundCheckDist, ground);
-        if (grounded && Input.GetKeyDown("space"))
-        {
-            body.AddForce(transform.up * jumpForce);
-        }
+        
     }
 
     void Update()
     {
-        
+        for (int i = 0; i < PublicVars.playerResources.Length; i++)
+        {
+            
+            if ((PublicVars.playerResources[i] >= 100) || (PublicVars.playerResources[i] <= 0)) {
+                SceneManager.LoadScene("GameOver");
+            } 
+            /*
+            if (PublicVars.playerResources[i] != 50)
+            {
+                SceneManager.LoadScene("GameOver");
+            } */
+        }
     }
 }
