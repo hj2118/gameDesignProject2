78545c5f046fa8baf11f86770b63994ebbddd8f9
hj2118 <hj2118@nyu.edu>
2023-09-23 12:54:30 -0400

    complete game loop

---
+++ project2/Assets/Code/QuitButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitButton : MonoBehaviour
+{
+    public void quitGame()
+    {
+        #if !UNITY_WEBGL
+        Application.Quit();
+        #endif
+    }
+
+}
