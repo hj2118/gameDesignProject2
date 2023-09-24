18e49c8db383a0c937bc6c6eae209d4243b390f6
mariaoliv <77213389+mariaoliv@users.noreply.github.com>
2023-09-24 13:07:39 -0400

    changes

--- project2/Assets/Code/PlayerCode.cs
+++ project2/Assets/Code/PlayerCode.cs
@@ -17,11 +17,15 @@ public class PlayerCode : MonoBehaviour
         for (int i = 0; i < PublicVars.playerResources.Length; i++)
         {
             
+<<<<<<< Updated upstream
             if ((PublicVars.playerResources[i] >= 100) || (PublicVars.playerResources[i] <= 0)) {
+=======
+            if ((PublicVars.Instance.playerResources[i] >= 200) || (PublicVars.Instance.playerResources[i] <= 0)) {
+>>>>>>> Stashed changes
                 SceneManager.LoadScene("GameOver");
             } 
             /*
-            if (PublicVars.playerResources[i] != 50)
+            if (PublicVars.Instance.playerResources[i] != 50)
             {
                 SceneManager.LoadScene("GameOver");
             } */
