576fc52a84481b607155ac7da4b5d1113ff9ee65
mariaoliv <77213389+mariaoliv@users.noreply.github.com>
2023-09-24 13:29:18 -0400

    changes to EnemyAI and PlayerCode scripts
    Also added GameObjects (EnemyAI and Player) so that these scripts can execute.

--- project2/Assets/Code/EnemyAI.cs
+++ project2/Assets/Code/EnemyAI.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyAI : MonoBehaviour
 {
-
+    public TextMeshProUGUI enemyAttackedText;
     float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyAttackedText.alpha = 0f;
     }
 
     // Update is called once per frame
@@ -17,25 +18,38 @@ public class EnemyAI : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if (timer > 4) {
-            int give_or_steal = Random.Range(0, 1);
-            int res_to_alter = Random.Range(0, 5);
-            int val_to_alter = Random.Range(1, 10);
-            if (give_or_steal == 0)
-            {
-                PublicVars.Instance.playerResources[res_to_alter] -= val_to_alter;
-                PublicVars.Instance.enemyResources[res_to_alter] += val_to_alter;
-            }
-            else
-            {
-                PublicVars.Instance.playerResources[res_to_alter] += val_to_alter;
-                PublicVars.Instance.enemyResources[res_to_alter] -= val_to_alter;
-            }
+        if (timer > 0.75)
+        {
+            enemyAttackedText.alpha = 0f;
+        }
+
+        //make "Enemy Attacked!" text appear for 0.75 seconds after the attack happens
+
+        if (timer > 25) {
+            AlterPlayerResources();
+            enemyAttackedText.alpha = 1f;
             timer = 0;
         }
 
 
        
+    }
+
+    void AlterPlayerResources()
+    {
+        int give_or_steal = Random.Range(0, 1);
+        int res_to_alter = Random.Range(0, 5);
+        int val_to_alter = Random.Range(1, 30);
+        if (give_or_steal == 0)
+        {
+            PublicVars.Instance.playerResources[res_to_alter] -= val_to_alter;
+            PublicVars.Instance.enemyResources[res_to_alter] += val_to_alter;
+        }
+        else
+        {
+            PublicVars.Instance.playerResources[res_to_alter] += val_to_alter;
+            PublicVars.Instance.enemyResources[res_to_alter] -= val_to_alter;
+        }
     }
 
     /*
